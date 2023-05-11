e8eebee2f079afff47b649c72950ad78419a304f
FR0614 <68939074+FR0614@users.noreply.github.com>
2023-05-11 13:59:30 +1000

    Update template.js

--- index_supporting_files/js/template.js
+++ index_supporting_files/js/template.js
@@ -1,3 +1,8 @@
+/*
+This code initializes the Headroom.js plugin with specific options and applies it to an HTML element with the class "headroom", 
+adding smooth scrolling effects to the header of a webpage.
+*/
+
 jQuery(document).ready(function($) {
 
 	$(".headroom").headroom({
